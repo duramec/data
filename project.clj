344a0556098141e197f52e2739bb5ebe990f2476
Aaron Bernard <aaron.bernard@duramec.com>
2013-06-20 19:35:58 -0500

    bumped version and edn-java dependency version

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject data "0.1.2"
-  :description "tools for working with EDN in text and binary"
-  :url "http://zig.io"
-  :license {:name "Copyright 2013 Duramec LLC"
-            :url "http://www.duramec.com" }
-  :plugins [[com.duramec/lein-scalac "0.1.1"]
-            [com.duramec/lein-scalatest "0.0.2"]]
-  :java-source-paths ["src/java"]
-  :scala-version "2.10.2"
-  :scala-test-paths ["test/scala"]
-  :dependencies [[org.fressian/fressian "0.6.3"]
-                 [us.bpsm/edn-java "0.4.2"]])
+(defproject data "0.1.5"
+            :description "tools for working with EDN in text and binary"
+            :url "http://zig.io"
+            :license {:name "Copyright 2013 Duramec LLC"
+                      :url "http://www.duramec.com" }
+            :profiles {:dev { :plugins [[com.duramec/lein-scalac "0.1.1"]
+                                        [com.duramec/lein-scalatest "0.0.3"]]
+                             :scala-test-paths ["test/scala"]}}
+            :java-source-paths ["src/java"]
+            :scala-version "2.10.2"
+            :dependencies [[org.fressian/fressian "0.6.3"]
+                           [us.bpsm/edn-java "0.4.3"]])
 
